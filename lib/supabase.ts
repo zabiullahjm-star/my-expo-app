26d48649989419899efb7a5c003aa349eb83a390
zabi <zabiullah.j.m@gmail.com>
2025-10-20 00:31:23 +0330

    acount roblem

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -9,6 +9,8 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
         storage: AsyncStorage,
         autoRefreshToken: true,
         persistSession: true,
-        detectSessionInUrl: false,
-    },
+        detectSessionInUrl: false,},
+        db: { schema: 'public'
+        }
+    
 });
